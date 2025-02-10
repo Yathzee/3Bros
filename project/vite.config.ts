e60a22a8636700896bbb69014fcdb471fb5c0d3c
Yathzee <ozkankahveci@outlook.com>
2025-02-09 22:20:20 -0600

    gh pages updated

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,7 +1,7 @@
+// filepath: /c:/Users/kahve/OneDrive/Desktop/3B/project/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/3Bros/',
   plugins: [react()],

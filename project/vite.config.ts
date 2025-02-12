947afd1fcc6719880d5d0b11358afd6bb9ae690d
Yathzee <ozkankahveci@outlook.com>
2025-02-11 21:51:57 -0600

    Updates

--- project/vite.config.ts
+++
@@ -1,8 +0,0 @@
-// filepath: /c:/Users/kahve/OneDrive/Desktop/3B/project/vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/3Bros/',
-  plugins: [react()],
-});
